a6e9c41436c2c866a6a898d8416ec762428b5b9d
Shaurin Bandivadekar <50839512+shaurin6@users.noreply.github.com>
2020-10-04 18:01:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -97,13 +97,7 @@ app.put('/image' , (req,res) => {
 
 
 
-// Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
+
 
 
 app.listen(3000 , () => {
